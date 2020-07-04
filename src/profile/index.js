77834ce72a2f372ac872b2c7250f1e3dafe5bc3c
dylantyates <dylan_yates1@homedepot.com>
2020-07-04 10:23:04 -0500

    chore: remove src

--- src/profile/index.js
+++
@@ -1,9 +0,0 @@
-import Name from './Name';
-import Experience from './Experience';
-import Chakra from './Chakra';
-
-export {
-  Name,
-  Experience,
-  Chakra,
-};
--- src/setupTests.js
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-import 'jest-enzyme';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
